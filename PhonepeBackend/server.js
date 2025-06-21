63927bea1d816d0201eeafd7834e9d51162ae1b4
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-06-21 13:14:01 +0530

    phonepe

--- PhonepeBackend/server.js
+++ PhonepeBackend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("I am phonepe backend server");
 });
 
 app.listen(3000, () => {
